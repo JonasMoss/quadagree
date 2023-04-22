c0f190a5fb902e3e6ca7f3c963c44d4bfe5ad7fc
Jonas Moss <jonas.gjertsen@gmail.com>
2023-04-22 08:06:49 +0200

    :wrench: Missing data.

--- workspace/klein2018.R
+++ workspace/klein2018.R
@@ -1,10 +1,5 @@
 fleissci(dat.klein2018)
+cohenci(dat.klein2018)
 
-x = as.matrix(dat.gwet2014)
-transform = "none"
-conf_level = 0.95
-fleiss = TRUE
-bootstrap = FALSE
-alternative = "two.sided"
-type = "adf"
-transform = "none"
+irrCAC::fleiss.kappa.raw(dat.klein2018, weights = "quadratic")
+irrCAC::conger.kappa.raw(dat.klein2018, weights = "quadratic")

0469281b762f2baf9c6ecefbfe7020227484bc6b
Jonas Moss <jonas.gjertsen@gmail.com>
2023-04-25 14:08:04 +0200

    :fire: Remove package docs.

--- R/quadagree.R
+++
@@ -1,5 +0,0 @@
-#' @description
-#' Package for quadratically weighted agreement coefficients. See `[quadagree]`
-#' for basic documentation and browse the vignettes for more details.
-#' `browseVignettes(package = "quadagree")`
-"_PACKAGE"

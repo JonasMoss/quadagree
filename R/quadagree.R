cff115c9d0baacec0fb6b4ba5b254cda3015c036
Jonas Moss <jonas.gjertsen@gmail.com>
2023-04-25 10:26:10 +0200

    :books: Add "package" docs.

---
+++ R/quadagree.R
@@ -0,0 +1,5 @@
+#' @description
+#' Package for quadratically weighted agreement coefficients. See `[quadagree]`
+#' for basic documentation and browse the vignettes for more details.
+#' `browseVignettes(package = "quadagree")`
+"_PACKAGE"
